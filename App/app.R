be63080414b68dda763d0b5e724a50189621e922
yangcheng <yangcheng@vt.edu>
2023-07-26 15:04:44 -0400

    Update the DSPG link in the Team

--- App/app.R
+++ App/app.R
@@ -1300,7 +1300,7 @@ ui <- navbarPage(#title = "DSPG 2023",
                                    h1(strong("Meet the Team")),
                                    br(),
                                    h4(strong("VT Data Science for the Public Good")),
-                                   p("The", a(href = 'https://dspg.aaec.vt.edu/', 'Data Science for the Public Good (DSPG) Young Scholars program', target = "_blank"),
+                                   p("The", a(href = ' https://dspg.aaec.vt.edu/', 'Data Science for the Public Good (DSPG) Young Scholars program', target = "_blank"),
                                      "is a summer immersive program held at the", a(href = 'https://aaec.vt.edu/index.html', 'Virginia Tech Department of Agricultural'), "and", a(href = 'https://ext.vt.edu/','Applied Economics and the Virginia Cooperative Extension Service.'),
                                      "In its third year, the program engages students from across the country to work together on projects that address state, federal, and local government challenges around critical
                                 social issues relevant in the world today. DSPG young scholars conduct research at the intersection of statistics, computation, and the social sciences to determine how 
